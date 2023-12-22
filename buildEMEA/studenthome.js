e38f40fad47a821ab572155a22f496871fb11bea
Fathimanasrinc <ftmnasrinc@gmail.com>
2023-12-22 17:35:53 +0530

    styled studentshome page

---
+++ buildEMEA/studenthome.js
@@ -0,0 +1,9 @@
+ function closeForm() {
+  document.getElementById("myDiv").style.display = "none";
+}
+
+
+function menu() {
+  document.getElementById("demo").innerHTML = document.getElementById("myDiv").style.display = "block";
+
+}
